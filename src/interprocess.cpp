18aa4b7618c896b81885e37452aaa55d875c1318
Pavel Miksha <89637241+keyboardcrash32@users.noreply.github.com>
2024-08-04 20:21:19 +0300

    Compile Interprocess functions only if running on Windows

--- src/interprocess.cpp
+++ src/interprocess.cpp
@@ -12,6 +12,7 @@ namespace Interprocess
 	
 	static void InitSvenSplitPipe()
 	{
+#ifdef PLATFORM_WINDOWS
 		pipe_SvenSplit = CreateNamedPipe(
 			"\\\\.\\pipe\\" SVENSPLIT_PIPE_NAME,
 			PIPE_ACCESS_OUTBOUND | FILE_FLAG_OVERLAPPED,
@@ -38,15 +39,19 @@ namespace Interprocess
 			CloseHandle(pipe_SvenSplit);
 			pipe_SvenSplit = INVALID_HANDLE_VALUE;
 		}
+#endif
 	}
 	
 	void Initialize()
 	{
+#ifdef PLATFORM_WINDOWS
 		InitSvenSplitPipe();
+#endif
 	}
 	
 	static void ShutdownSvenSplitPipe()
 	{
+#ifdef PLATFORM_WINDOWS
 		if (pipe_SvenSplit != INVALID_HANDLE_VALUE)
 		{
 			CloseHandle(pipe_SvenSplit);
@@ -56,15 +61,19 @@ namespace Interprocess
 	
 		CloseHandle(overlapped.hEvent);
 		std::memset(&overlapped, 0, sizeof(overlapped));
+#endif
 	}
 	
 	void Shutdown()
 	{
+#ifdef PLATFORM_WINDOWS
 		ShutdownSvenSplitPipe();
+#endif
 	}
 	
 	static void WriteSvenSplit(const std::vector<char>& data)
 	{
+#ifdef PLATFORM_WINDOWS
 		if (pipe_SvenSplit == INVALID_HANDLE_VALUE)
 			return;
 	
@@ -121,19 +130,23 @@ namespace Interprocess
 				return;
 			}
 		}
+#endif
 	}
 	
 	static size_t AddTimeToBuffer(char* buf, const Time& time)
 	{
+#ifdef PLATFORM_WINDOWS
 		std::memcpy(buf, &time.hours, sizeof(time.hours));
 		std::memcpy(buf + sizeof(time.hours), &time.minutes, sizeof(time.minutes));
 		std::memcpy(buf + sizeof(time.hours) + sizeof(time.minutes), &time.seconds, sizeof(time.seconds));
 		std::memcpy(buf + sizeof(time.hours) + sizeof(time.minutes) + sizeof(time.seconds), &time.milliseconds, sizeof(time.milliseconds));
 		return sizeof(time.hours) + sizeof(time.minutes) + sizeof(time.seconds) + sizeof(time.milliseconds);
+#endif
 	}
 	
 	void WriteTime(const Time& time)
 	{
+#ifdef PLATFORM_WINDOWS
 		if (!g_lpHUDTimer->interprocess_enable->value)
 			return;
 
@@ -156,10 +169,12 @@ namespace Interprocess
 		{
 			WriteSvenSplit(buf);
 		}
+#endif
 	}
 	
 	void WriteGameEnd(const Time& time)
 	{
+#ifdef PLATFORM_WINDOWS
 		std::vector<char> buf(11);
 		buf[0] = static_cast<char>(buf.size());
 		buf[1] = static_cast<char>(MessageType::EVENT);
@@ -167,10 +182,12 @@ namespace Interprocess
 		AddTimeToBuffer(buf.data() + 3, time);
 	
 		WriteSvenSplit(buf);
+#endif
 	}
 	
 	void WriteMapChange(const Time& time, const std::string& map)
 	{
+#ifdef PLATFORM_WINDOWS
 		int32_t size = static_cast<int32_t>(map.size());
 	
 		std::vector<char> buf(15 + size);
@@ -183,10 +200,12 @@ namespace Interprocess
 		std::memcpy(buf.data() + 3 + time_size + 4, map.data(), size);
 	
 		WriteSvenSplit(buf);
+#endif
 	}
 	
 	void WriteTimerReset(const Time& time)
 	{
+#ifdef PLATFORM_WINDOWS
 		std::vector<char> buf(11);
 		buf[0] = static_cast<char>(buf.size());
 		buf[1] = static_cast<char>(MessageType::EVENT);
@@ -194,10 +213,12 @@ namespace Interprocess
 		AddTimeToBuffer(buf.data() + 3, time);
 	
 		WriteSvenSplit(buf);
+#endif
 	}
 	
 	void WriteTimerStart(const Time& time)
 	{
+#ifdef PLATFORM_WINDOWS
 		std::vector<char> buf(11);
 		buf[0] = static_cast<char>(buf.size());
 		buf[1] = static_cast<char>(MessageType::EVENT);
@@ -205,10 +226,12 @@ namespace Interprocess
 		AddTimeToBuffer(buf.data() + 3, time);
 	
 		WriteSvenSplit(buf);
+#endif
 	}
 	
 	void WriteSSALeapOfFaith(const Time& time)
 	{
+#ifdef PLATFORM_WINDOWS
 		std::vector<char> buf(11);
 		buf[0] = static_cast<char>(buf.size());
 		buf[1] = static_cast<char>(MessageType::EVENT);
@@ -216,10 +239,12 @@ namespace Interprocess
 		AddTimeToBuffer(buf.data() + 3, time);
 	
 		WriteSvenSplit(buf);
+#endif
 	}
 
 	Interprocess::Time GetTime()
 	{
+#ifdef PLATFORM_WINDOWS
 		int hours = g_RTATimer.GetHours();
 		int minutes = g_RTATimer.GetMinutes();
 		int seconds = g_RTATimer.GetSeconds();
@@ -231,5 +256,6 @@ namespace Interprocess
 			static_cast<uint8_t>(seconds),
 			static_cast<uint16_t>(milliseconds)
 		};
+#endif
 	}
 }
